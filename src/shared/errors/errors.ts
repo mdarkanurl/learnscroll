b72b6938e5638493265a019fcd081aafbf13f18c
Mohammad Arkan <mdarkanurl@gmail.com>
2026-05-29 22:30:11 +0600

    Added custom error class

---
+++ src/shared/errors/errors.ts
@@ -0,0 +1,7 @@
+export class CustomError extends Error {
+    constructor(message: string, public statusCode: number) {
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+}
---
+++ src/shared/errors/index.ts
@@ -0,0 +1,2 @@
+import { CustomError } from "./errors";
+export default CustomError;
