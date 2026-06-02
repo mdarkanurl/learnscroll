d76f879abb024dc9798768718369ade93631f1e4
Mohammad Arkan <mdarkanurl@gmail.com>
2026-06-02 21:01:01 +0600

    Called rabbitmq from app bootstrap function

--- src/bootstrap/server.ts
+++ src/bootstrap/server.ts
@@ -1,8 +1,20 @@
 import app from './app'
+import { rabbitmq } from "#rabbitmq";
 
-Bun.serve({
-  port: process.env.PORT ?? 3000,
-  fetch: app.fetch,
+async function bootstrap() {
+  // Connect RabbitMQ
+  await rabbitmq();
+
+  // Start HTTP server
+  Bun.serve({
+    port: process.env.PORT ?? 3000,
+    fetch: app.fetch,
+  });
+
+  console.log('Server running on port 3000');
+}
+
+bootstrap().catch((err) => {
+  console.error("Failed to start application:", err);
+  process.exit(1);
 });
-
-console.log('Server running on port 3000');
