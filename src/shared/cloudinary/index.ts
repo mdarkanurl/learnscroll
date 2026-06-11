0c06b27f88b47fb713f3e5358f0c104471eb9c31
Mohammad Arkan <mdarkanurl@gmail.com>
2026-06-11 10:57:31 +0600

    Added generateApiSignRequest function

--- src/shared/cloudinary/index.ts
+++ src/shared/cloudinary/index.ts
@@ -1,7 +1,7 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as Cloudinary } from 'cloudinary';
 import { env } from '#configs';
 
-cloudinary.config({
+Cloudinary.config({
   cloud_name: env.cloudinaryCloudName,
   api_key: env.cloudinaryApiKey,
   api_secret: env.cloudinaryApiSecret,
@@ -9,7 +9,7 @@ cloudinary.config({
 
 export async function uploadImage(buffer: Buffer, folder: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    const uploadStream = cloudinary.uploader.upload_stream(
+    const uploadStream = Cloudinary.uploader.upload_stream(
       { folder },
       (error, result) => {
         if (error) reject(error);
@@ -19,3 +19,11 @@ export async function uploadImage(buffer: Buffer, folder: string): Promise<strin
     uploadStream.end(buffer);
   });
 }
+
+export async function generateApiSignRequest(payload: Record<string, string>): Promise<string> {
+  return Cloudinary.utils.api_sign_request(
+    payload,
+    env.cloudinaryApiSecret,
+  );
+}
+
